fa7a9a6ee1e99fd64341f528e639b057e765dfeb
lvisei <liuvigongzuoshi@foxmail.com>
2022-05-04 23:43:48 +0800

    test: rename images file for test (#7)

--- util_test.go
+++ util_test.go
@@ -10,12 +10,12 @@ import (
 // TestLoadImage test open different type image file
 func TestLoadImage(t *testing.T) {
 	assertions := assert.New(t)
-	jpgFilename := "testdata/earth_5568 × 3712.jpg"
+	jpgFilename := "testdata/earth_5568*3712.jpg"
 	openedImage, err := image2tiles.LoadImage(jpgFilename)
 	assertions.True(err == nil, "jpg image format should be supported")
 	assertions.True(openedImage != nil, "opened jpg file should not be nil")
 
-	pngFilename := "testdata/spongebob_698 × 530.png"
+	pngFilename := "testdata/spongebob_698*530.png"
 	openedImage, err = image2tiles.LoadImage(pngFilename)
 	assertions.True(err == nil, "png image format should be supported")
 	assertions.True(openedImage != nil, "opened jpg file should not be nil")
